d4ae79c4a27f1188b3c6639d581a68b90083b0c4
Manuel Pautasso <manuelpautasso@gmail.com>
2022-07-19 15:47:47 -0300

    fix name variable global del url de la BD

--- src/main/java/mp/crud/enums/VariablesGlobales.java
+++ src/main/java/mp/crud/enums/VariablesGlobales.java
@@ -1,9 +1,9 @@
 package mp.crud.enums;
 
 public enum VariablesGlobales {
-    BD_URL(System.getenv("BD_URL") == null ? 
+    BD_URL(System.getenv("CLEARDB_DATABASE_URL") == null ? 
             "jdbc:mysql://root:admin@localhost:3306/cac_crud_biblioteca?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true" 
-            : "jdbc:" + System.getenv("BD_URL") + "&useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true")    
+            : "jdbc:" + System.getenv("CLEARDB_DATABASE_URL") + "&useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true")    
     ;
 
     private final String value;
